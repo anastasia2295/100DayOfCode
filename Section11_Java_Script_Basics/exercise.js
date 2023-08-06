7187addcc81a4c177a8d59efbb54c0df9c302944
nastay <nastay5@mail.ru>
2023-08-06 16:17:42 +0300

    paractice day 32

---
+++ Section11_Java_Script_Basics/exercise.js
@@ -0,0 +1,6 @@
+let onlainCourse = "Course";
+alert(onlainCourse);
+let priceOfCourse = 100;
+alert(priceOfCourse);
+let  threeMainGoals = ["Self-development", "Money", "New job"]; 
+alert(threeMainGoals);
