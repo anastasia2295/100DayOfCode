6b8203ab0c2513cb92c66a452be7d93c2ce3de17
nastay <nastay5@mail.ru>
2023-08-06 16:52:39 +0300

    practice day 32,solucion

--- Section11_Java_Script_Basics/exercise.js
+++ Section11_Java_Script_Basics/exercise.js
@@ -1,6 +1,28 @@
-let onlainCourse = "Course";
-alert(onlainCourse);
-let priceOfCourse = 100;
-alert(priceOfCourse);
-let  threeMainGoals = ["Self-development", "Money", "New job"]; 
-alert(threeMainGoals);
+//let courseName = "Web development - The comltete Cuide";
+//let coursePrice = 39;
+//let courseGoals = ["Learn web development", "Become a web developer","Have a fun"];
+
+//alert(courseName);
+//alert(coursePrice);
+//alert(courseGoals);
+
+
+let onlineCourse = {
+    name: "Web development - The comltete Cuide", 
+    price: 39,
+    goals: ["Learn web development", "Become a web developer","Have a fun"]
+};
+alert(onlineCourse.name);
+alert(onlineCourse.price);
+alert(onlineCourse.goals);
+
+alert(onlineCourse.goals[1])
+
+
+function getListItem (array, arrayIndex) {
+    let arrayElement = array[arrayIndex]
+    return arrayElement
+}
+
+let firstGoals = getListItem(onlineCourse.goals, 0);
+alert(firstGoals)
