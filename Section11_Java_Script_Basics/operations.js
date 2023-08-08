735288ddf4abf4d5ea2041067461c38d00865c36
nastay <nastay5@mail.ru>
2023-08-08 18:17:17 +0300

    result +=5; //result = recult +5//

---
+++ Section11_Java_Script_Basics/operations.js
@@ -0,0 +1,15 @@
+console.log(10+4);
+console.log(10-4);
+console.log(10*4);
+console.log(10/4);
+console.log(10%4);
+console.log(10%6);
+
+console.log((10+4-5)*10);
+
+let result = (10+4-5)*10;
+result = 10 * 4;
+
+result++; //result = result +1//
+result +=5; //result = recult +5//
+console.log(result);
