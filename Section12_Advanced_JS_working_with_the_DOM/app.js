09bb41ee53ee4104e42affe706a961ba7b7b4a65
nastay <nastay5@mail.ru>
2023-08-10 09:12:58 +0300

    let anchorElement = document.getElementById(external-link);

--- Section12_Advanced_JS_working_with_the_DOM/app.js
+++ Section12_Advanced_JS_working_with_the_DOM/app.js
@@ -1,2 +1,6 @@
-document.body.children[1].children[0].href = "https://www.google.com/?hl=ru";
-console.dir(document);
+// document.body.children[1].children[0].href = "https://www.google.com/?hl=ru";
+
+// console.dir(document);
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://www.google.com/?hl=ru";
