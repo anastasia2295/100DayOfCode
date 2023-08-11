27009596d93110148e6d74923ab617d2ecbe588c
nastay <nastay5@mail.ru>
2023-08-11 21:38:12 +0300

    day 36    let enteredText = event.target.value

--- Section12_Advanced_JS_working_with_the_DOM/app2.js
+++ Section12_Advanced_JS_working_with_the_DOM/app2.js
@@ -5,4 +5,17 @@ function changeParagraphText() {
     paragraphElement.textContent = "Clicked"
 }
 
- paragraphElement.addEventListener("click", changeParagraphText)
+ paragraphElement.addEventListener("click", changeParagraphText);
+
+
+ let inputElement = document.querySelector("input");
+
+ function refrieveUserInput(event) {
+   // let enteredText = inputElement.value
+   let enteredText = event.target.value
+   // let enteredText = event.data показывает только введеное значение
+    console.log(enteredText)
+  //  console.log(event)
+ }
+inputElement.addEventListener("input", refrieveUserInput);
+
