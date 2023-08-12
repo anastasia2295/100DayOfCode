6a5ec2a6b876de3ca5aa32b170a862706be3eef1
nastay <nastay5@mail.ru>
2023-08-12 10:24:26 +0300

    day 36 60/60 js

---
+++ Section12_Advanced_JS_working_with_the_DOM/demo.js
@@ -0,0 +1,19 @@
+let productNameInputElement = document.getElementById("product-name");
+let remainingCharsElement = document.getElementById("remaining-chars");
+
+// console.dir(productUserInputElement)
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCaracters(event) {
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+
+    let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+
+}
+
+
+productNameInputElement.addEventListener("input", updateRemainingCaracters);
