db90473ce5527067a2eb297713ddfc81b565c801
nastay <nastay5@mail.ru>
2023-08-16 19:59:54 +0300

    loop while

--- Section13_Control_Structures/loops.js
+++ Section13_Control_Structures/loops.js
@@ -17,3 +17,9 @@ for (const propertyName in loggedInUser){
     console.log(loggedInUser[propertyName])
     
 }
+
+let isFinished = false
+while (!isFinished) {
+isFinished = confirm("Do you want to quit?")
+}
+console.log("Done")
