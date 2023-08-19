289ee5c0fcbbe4608a971905c3a2b39431f2ba2c
nastay <nastay5@mail.ru>
2023-08-19 22:18:16 +0300

    loop while parte 2

--- Section13_Control_Structures/loops2.js
+++ Section13_Control_Structures/loops2.js
@@ -61,26 +61,40 @@ displayUserDataButtonElement.addEventListener("click", dispayUserData)
 
 
 
-const rollDiceButtonElement = document.querySelector("#statistics button")
+const rollDiceButtonElement = document.querySelector('#statistics button');
 
 function rollDice() {
-
+  return Math.floor(Math.random() * 6) + 1; // Math.floor(): 5.64 => 5
 }
 
 function deriveNumberOfDiceRolls() {
-    const targetNumberInputElement = document.getElementById("user-targer-number")
-    const diceRollsListElement = document.getElementById("dice-rolls")
-    const enteredNumber = targetNumberInputElement.value
-    diceRollsListElement.innerHTML = "";
+  const targetNumberInputElement = document.getElementById('user-target-number');
+  const diceRollsListElement = document.getElementById('dice-rolls');
 
-    let hasRolledTargetNumber = false;
+  const enteredNumber = targetNumberInputElement.value;
+  diceRollsListElement.innerHTML = '';
 
-   while (!hasRolledTargetNumber) {
-    const rollNumber = rollDice()
-    if (rollNumber == enteredNumber)
-    hasRolledTargetNumber = true
-   } 
+  let hasRolledTargetNumber = false;
+  let numberOfRolls = 0;
+
+  while (!hasRolledTargetNumber) {
+    const rolledNumber = rollDice();
+    // if (rolledNumber == enteredNumber) {
+    //   hasRolledTargetNumber = true;
+    // }
+    numberOfRolls++;
+    const newRollListItemElement = document.createElement('li');
+    const outputText = 'Roll ' + numberOfRolls + ': ' + rolledNumber;
+    newRollListItemElement.textContent = outputText;
+    diceRollsListElement.append(newRollListItemElement);
+    hasRolledTargetNumber = rolledNumber == enteredNumber;
+  }
+
+  const outputTotalRollsElement = document.getElementById('output-total-rolls');
+  const outputTargetNumberElement = document.getElementById('output-target-number');
+
+  outputTargetNumberElement.textContent = enteredNumber;
+  outputTotalRollsElement.textContent = numberOfRolls;
 }
 
-
-rollDiceButtonElement.addEventListener("click", deriveNumberOfDiceRolls)
+rollDiceButtonElement.addEventListener('click', deriveNumberOfDiceRolls);
