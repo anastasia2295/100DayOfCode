3a4091bf6f987cc01eb386dadfef115eec592f8f
nastay <nastay5@mail.ru>
2023-08-20 21:35:18 +0300

    calculator parte4

--- Section13_Control_Structures/calculator.js
+++ Section13_Control_Structures/calculator.js
@@ -1,7 +1,6 @@
 const numberElement1 = document.getElementById("user-number1")
 const numberElement2 = document.getElementById("user-number2")
 let resultNumberElement = document.getElementById("result")
-console.dir(document)
 
 const additionButton = document.getElementById("addition")
 const subtractionButton = document.getElementById("subtraction")
@@ -9,9 +8,9 @@ const multiplicationButton = document.getElementById("multiplication")
 const divisionButton = document.getElementById("division")
 
 function calculatedAdd(){
-const enteredNumber1 = numberElement1.value 
-const enteredNumber2 = numberElement2.value 
-  resultNumberElement.textContent = + enteredNumber1 + enteredNumber2
+const enteredNumber1 = +numberElement1.value 
+const enteredNumber2 = +numberElement2.value 
+  resultNumberElement.textContent =  enteredNumber1 + enteredNumber2
 
 }
 
